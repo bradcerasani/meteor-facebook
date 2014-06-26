f183a3310017b93eb410b50bf3680750648f90f9
Brad Cerasani <bradcerasani@gmail.com>
2014-06-26 00:15:40 -0500

    bug fixes

--- client/config/config.js
+++ client/config/config.js
@@ -1,7 +1,5 @@
 Accounts.ui.config({
     requestPermissions: {
-        facebook: ['email', 'user_friends', 'user_location', 'user_events',
-            'friends_events', 'friends_location', 'friends_about_me',
-            'user_status', 'friends_status', 'read_friendlists'],
+        facebook: ['email', 'user_friends', 'friends_events', 'friends_location', 'friends_about_me', 'friends_status'],
     }
 });
--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,6 +1,7 @@
 Template.home.events({
   'click #btn-user-data': function(e) {
     Meteor.call('getUserData', function(err, data) {
+      console.log("hello");
       $('#result').text(JSON.stringify(data, undefined, 4));
     });
   }
