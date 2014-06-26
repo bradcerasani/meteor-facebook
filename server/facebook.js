3030b5af227e0cfc662f91a73a6d90580af97fa4
Brad Cerasani <bradcerasani@gmail.com>
2014-06-25 23:49:33 -0500

    fbgraph package and server method

---
+++ server/facebook.js
@@ -0,0 +1,22 @@
+function Facebook(accessToken) {
+  this.fb = Meteor.require('fbgraph');
+  this.accessToken = accessToken;
+  this.fb.setAccessToken(this.accessToken);
+  this.options = {
+    timeout: 3000,
+    pool: {maxSockets: Infinity},
+    headers: {connection: "keep-alive"}
+  }
+  this.fb.setOptions(this.options);
+}
+
+Facebook.prototype.query = function(query, method) {
+  var self = this;
+  var method = (typeof method === 'undefined') ? 'get' : method;
+  var data = Meteor.sync(function(done) {
+    self.fb[method](query, function(err, res) {
+      done(null, res);
+    });
+  });
+  return data.result;
+}
